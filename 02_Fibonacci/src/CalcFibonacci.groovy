9130cbda56585bebf61a2cdd2a6ea7926ec27713
Artem Mukhachev <artem@sinor.ru>
2017-02-08 01:19:21 +0700

    Вычисление числа Фибоначчи в цикле

---
+++ 02_Fibonacci/src/CalcFibonacci.groovy
@@ -0,0 +1,33 @@
+class Fibonacci{
+    static long firstFib = 0;
+    static long secondFib = 1;
+
+    static long getN(int level) {
+        long fib, fib_prev1, fib_prev2
+        if (level <= 1){
+            return firstFib
+        }
+        if (level == 2) {
+            return secondFib
+        }
+        fib_prev2 = firstFib
+        fib_prev1 = secondFib
+        for (int i = 3; i <= level; i++){
+            fib = fib_prev1 + fib_prev2
+            fib_prev2 = fib_prev1
+            fib_prev1 = fib
+        }
+        return fib
+    }
+}
+
+assert Fibonacci.getN(-20) == 0
+assert Fibonacci.getN(0) == 0
+assert Fibonacci.getN(1) == 0
+assert Fibonacci.getN(2) == 1
+assert Fibonacci.getN(3) == 1
+assert Fibonacci.getN(4) == 2
+assert Fibonacci.getN(5) == 3
+assert Fibonacci.getN(6) == 5
+
+println Fibonacci.getN(50)
